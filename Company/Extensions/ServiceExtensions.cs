9f785e8cb53c9cf04734acb6fe5978dfb5e306cd
Youssef Adel <e@g.com>
2023-09-10 12:24:28 +0300

    Adding Essential Services and Middlewares

---
+++ Company/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+namespace Company.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }
+            );
+        });
+    }
+
+    public static void ConfigureIISIntegration(this IServiceCollection services)
+    {
+        services.Configure<IISOptions>(options =>
+        {
+
+        }
+        );
+    }
+}
