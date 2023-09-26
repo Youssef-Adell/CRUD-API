6c67ba2f05649ce356796dce75a994b2a0101ee4
Youssef Adel <al3nkpout2@gmail.com>
2023-09-26 03:17:19 +0300

    Organize and comment the code of Custom model binder

--- web/ModelBinders/IEnumerableOfTModelBinder.cs
+++ web/ModelBinders/IEnumerableOfTModelBinder.cs
@@ -15,7 +15,7 @@ public class IEnumerableOfTModelBinder : IModelBinder
             return Task.CompletedTask;
         }
 
-        // Get the Value from the request and ensure that it is not null
+        #region Get the Value from the request and ensure that it is not null
         string? providedValue = bindingContext.ValueProvider
                                 .GetValue(bindingContext.ModelName)
                                 .FirstValue;
@@ -25,19 +25,25 @@ public class IEnumerableOfTModelBinder : IModelBinder
             bindingContext.Result = ModelBindingResult.Success(null);
             return Task.CompletedTask;
         }
+        #endregion
 
-        // Convert the ProvidedValue type to the needed model type
+        #region Convert the ProvidedValue type to the needed model type
+        // Getting the T in IEnumarable<T> which is the type of action parameter need to be bound 
         var genericTypeInIEnumarable = bindingContext.ModelType.GenericTypeArguments[0];
 
         TypeConverter converter = TypeDescriptor.GetConverter(genericTypeInIEnumarable);
 
+        // Converting the string provided in the request to array of strings then convert this array to array of T type
+        // but unfortunately the converter method returns object type not T type
         var convertedValueObjects = providedValue.Split(",", StringSplitOptions.TrimEntries)
                                             .Select(idStr => converter.ConvertFromString(idStr))
                                             .ToArray();
 
+        // So we created another array of T type and copy the previous array of Objects to it
         var convertedValueActualType = Array.CreateInstance(genericTypeInIEnumarable, convertedValueObjects.Length);
         convertedValueObjects.CopyTo(convertedValueActualType, 0);
 
+        #endregion
 
         bindingContext.Result = ModelBindingResult.Success(convertedValueActualType);
         return Task.CompletedTask;
