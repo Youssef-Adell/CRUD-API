927f9adfca599a701080b1d53b7c71e52b28963d
Youssef Adel <al3nkpout2@gmail.com>
2023-10-01 23:14:10 +0300

    Add Custom Validation ActionFilter

---
+++ web/Filters/ValidationFilterAttribute.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Web.ActionFilters;
+
+public class ValidationFilterAttribute : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        //check if the data sent by client is valid or not
+        if (!context.ModelState.IsValid)
+            context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+    }
+
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
