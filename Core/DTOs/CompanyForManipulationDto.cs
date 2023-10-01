86c7ab0a0c2f9ebab688822131a46af6dca98ce7
Youssef Adel <al3nkpout2@gmail.com>
2023-10-02 01:55:19 +0300

    Move common code of CompanyForCreationDto and CompanyForUpdateDto to abstract record and inherit from it

---
+++ Core/DTOs/CompanyForManipulationDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.DTOs;
+
+public abstract record CompanyForManipulationDto
+{
+    [Required(ErrorMessage = "Company Name is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
+    public string Name { get; init; }
+
+    [Required(ErrorMessage = "Address is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Address is 30 characters.")]
+    public string Address { get; init; }
+
+    [Required(ErrorMessage = " Country is a required field.")]
+    [MaxLength(30, ErrorMessage = "Maximum length for the Country is 30 characters.")]
+    public string Country { get; init; }
+
+    public IEnumerable<EmployeeForCreationDto> Employees { get; init; }
+}
--- Core/DTOs/CompanyForUpdateDto.cs
+++ Core/DTOs/CompanyForUpdateDto.cs
@@ -1,9 +1,3 @@
 namespace Core.DTOs;
 
-public record CompanyForUpdatenDto
-{
-    public string Name { get; init; }
-    public string Address { get; init; }
-    public string Country { get; init; }
-    public IEnumerable<EmployeeForCreationDto> Employees { get; init; }
-}
+public record CompanyForUpdatenDto : CompanyForManipulationDto;
