df10f9610964cb4713dda6883555aa66decd1df9
Youssef Adel <al3nkpout2@gmail.com>
2023-10-02 06:49:59 +0300

    Refactor Validation Filer to make it check for null argument

--- web/ActionFilters/ValidationFilterAttribute.cs
+++ web/ActionFilters/ValidationFilterAttribute.cs
@@ -7,9 +7,26 @@ public class ValidationFilterAttribute : IActionFilter
 {
     public void OnActionExecuting(ActionExecutingContext context)
     {
-        //check if the data sent by client is valid or not
+        #region return BadRequest response if the Dto argument sent to the action is null 
+        // these values are added by Routing Middleware after processing the incoming request
+        var controllerName = context.RouteData.Values["controller"];
+        var actionName = context.RouteData.Values["action"];
+
+        // the default implemention of ToString() contains tha name of the type
+        var actionArgument = context.ActionArguments.Values
+            .SingleOrDefault(arg => arg.ToString().Contains("Dto"));
+
+        if (actionArgument is null)
+        {
+            context.Result = new BadRequestObjectResult($"Object is null, Controller:{controllerName}, Action:{actionName}");
+            return;
+        }
+        #endregion
+
+        #region return UnprocessableEntity response if the data sent by client is invalid
         if (!context.ModelState.IsValid)
             context.Result = new UnprocessableEntityObjectResult(context.ModelState);
+        #endregion
     }
 
     public void OnActionExecuted(ActionExecutedContext context)
