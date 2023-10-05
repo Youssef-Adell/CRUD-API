0748e44540f8a5435157cbde1152d456b58e635c
Youssef Adel <al3nkpout2@gmail.com>
2023-10-05 09:25:38 +0300

    Add Filtering by age to GetEmployeesForCompany endpoint

--- Core/DTOs/EmployeeParameters.cs
+++ Core/DTOs/EmployeeParameters.cs
@@ -2,4 +2,6 @@ namespace Core.DTOs;
 
 public class EmployeeParameters : RequestParameters
 {
+    public int MinAge { get; set; } = int.MinValue;
+    public int MaxAge { get; set; } = int.MaxValue;
 }
--- Infrastructure/Data/Repositories/EmployeeRepository.cs
+++ Infrastructure/Data/Repositories/EmployeeRepository.cs
@@ -14,7 +14,11 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
     {
         List<Employee> employees =
-            await FindByCondition((e) => e.CompanyId.Equals(companyId), trackChanges)
+            await FindByCondition((e) =>
+            e.CompanyId.Equals(companyId) &&
+            e.Age >= employeeParameters.MinAge &&
+            e.Age <= employeeParameters.MaxAge
+            , trackChanges)
             .OrderBy(e => e.Name)
             .Skip((employeeParameters.PageNumber - 1) * employeeParameters.PageSize)
             .Take(employeeParameters.PageSize)
