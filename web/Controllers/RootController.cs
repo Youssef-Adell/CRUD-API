340d8e5d44f0615eb63144734174828977bc2927
Youssef Adel <al3nkpout2@gmail.com>
2023-10-20 01:14:16 +0300

    Add Root Document

---
+++ web/Controllers/RootController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Core.Entities.LinkModels;
+
+namespace Web.Controllers;
+
+[Route("api")]
+[ApiController]
+public class RootController : Controller
+    {
+        private LinkGenerator _linkGenerator;
+
+        public RootController(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet(Name ="GetRoot")]
+        public IActionResult GetRoot()
+        {
+            List<Link> links = new List<Link>
+            {
+                new Link{
+                    Href=_linkGenerator.GetPathByName(HttpContext, nameof(GetRoot)),
+                    Rel="self",
+                    Method="GET"
+                },
+                new Link{
+                    Href=_linkGenerator.GetPathByName(HttpContext, "GetAllCompanies"),
+                    Rel="get_companies",
+                    Method="GET"
+                },
+                new Link{
+                    Href=_linkGenerator.GetPathByName(HttpContext, "CreateCompany"),
+                    Rel="create-company",
+                    Method="POST"
+                }
+            };
+
+            return Ok(links);
+        }
+    }
